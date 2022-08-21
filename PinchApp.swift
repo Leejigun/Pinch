c92f93f82080d13da5d1af43e9931b049f6b896b
bimo.ez <bimo.ez@kakaomobility.com>
2022-08-21 16:53:22 +0900

    프로젝트 기본 설정

---
+++ PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by bimo.ez on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by bimo.ez on 2022/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
